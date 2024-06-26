1126734b53773268bea72e34906246b787ae099f
hamdani123 <udadani14@gmail.com>
2024-06-26 13:43:06 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,7 @@
         <h2>COBA TEST</h2>
         <h5>cobaaaaa</h5>
         <h3>testing</h3>
+        <h1>reererere</h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
